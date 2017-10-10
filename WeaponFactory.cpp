e232df55bb33ac3b13029c9baa300f4fe4472095
sebastiansoffici <sebastiansoffici@wc-dhcp112d180.student-secure.wireless.fsu.edu>
2017-10-10 13:22:47 -0400

    Added weapons to WeaponFactory

--- WeaponFactory.cpp
+++ WeaponFactory.cpp
@@ -9,6 +9,9 @@
 #include "WeaponFactory.h"
 #include "CommonSword.h" 
 #include "CommonSpear.h"
+#include "SimpleAxe.h"
+#include "CrazyRandomSword.h"
+#include "BlindTomahawk.h"
 
 WeaponFactory* WeaponFactory::instance = NULL;
 
@@ -20,13 +23,30 @@ WeaponFactory * WeaponFactory::getInstance() {
 }
 
 Weapon * WeaponFactory::getWeapon(std::string name) {
-    if (name.compare("sword") == 0) {
-        return new CommonSword();
-    }
-    
-    if (name.compare("spear") == 0) {
-        return new CommonSpear();
-    }
+	if (name.compare("random") == 0)
+	{
+		return new CrazyRandomSword();
+	}
+
+	if (name.compare("axe") == 0)
+	{
+		return new SimpleAxe();
+	}
+
+	if (name.compare("tomahawk") == 0)
+	{
+		return new BlindTomahawk();
+	}
+	if (name.compare("sword") == 0 && name.compare("random") != 0) 
+	{
+        	return new CommonSword();
+    	}
+
+	if (name.compare("spear") == 0) 
+	{
+        	return new CommonSpear();
+	}
+
 
     throw "Invalid weapon";
 }
